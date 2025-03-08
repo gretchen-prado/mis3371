90c6f69e7abfb39968c3803cb7eaadd9a71fbb84
gretchen-prado <gretchenprado2@gmail.com>
2025-03-07 22:59:16 -0600

    Create scripts-2.js

---
+++ scripts-2.js
@@ -0,0 +1,12 @@
+ /* 
+Program name: index-2.html this is scripts-2.js
+Author: Gretchen R Prado Gonzalez
+Date created: February 5
+Date last edited: March 7
+Version: 2.0
+Description: JavaScript for patient registration form.*/
+
+
+/*from w3schools*/
+const d = new Date();
+document.getElementById("datetoday").innerHTML = d;
